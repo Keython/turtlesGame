9ecb0327854406b2a312ce2d7930d66145928a6d
Keython <keita.laimina@gmail.com>
2022-03-09 00:03:19 +0200

    skelets

---
+++ main.py
@@ -0,0 +1,39 @@
+print("Hello, gamer!")
+#vārdi, sākums
+
+
+#taustiņu pārbaude
+
+
+
+#kontrolpunktu faila nolasīšana
+
+
+#kontrolpunktu tabula
+
+
+#indentifikatora atbilstības pārbaude
+
+
+#akumolatora uzskaite, a0=100%
+
+
+#laikapstākļu faktors mašīnas nobraukumam
+
+
+#laikapstākļu ģenerators
+
+
+#uzlādes līmeņa, km nolasīšana jebkurā laikā
+
+
+#10 kotrolpunkti, uzlādes stacija, uzdevumi
+
+
+#bonusa punkti auto uzlādei par atrisinātu uzdevumu
+
+
+#evakuators
+
+
+#spēles beigas, rezultātu izvade

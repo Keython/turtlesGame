fcaa860d61f438145e817ede88d1b96de8090a62
Keython <keita.laimina@gmail.com>
2022-03-24 16:02:13 +0200

    būsOkSuperVissIetUzPriekšu

--- main.py
+++ main.py
@@ -13,146 +13,19 @@ print("starts")
         #print("STARTS")
         #break
 # Reading an excel file using Python
-import xlrd
- 
-# Give the location of the file
-loc = ("faili/kontrolpunktu tabula (bez km).xlsx")
- 
-# To open Workbook
-wb = xlrd.open_workbook(loc)
-sheet = wb.sheet_by_index(0)
-#for i in range(sheet.ncols):
-#    print(sheet.cell_value(0, i))
- 
-# For row 0 and column 0
-ab =sheet.cell_value(2, 1)
-ac = sheet.cell_value(3, 1)
-ad = sheet.cell_value(4, 1)
-ae = sheet.cell_value(5, 1)
-af = sheet.cell_value(6, 1)
-ag = sheet.cell_value(7, 1)
-ah = sheet.cell_value(8, 1)
-ai = sheet.cell_value(9, 1)
-aj = sheet.cell_value(10, 1)
-a1 = sheet.cell_value(11, 1)
-a2 = sheet.cell_value(12, 1)
-a3 = sheet.cell_value(13, 1)
-
-ba =sheet.cell_value(1, 2)
-bc =sheet.cell_value(3, 2)
-bd = sheet.cell_value(4, 2)
-be = sheet.cell_value(5, 2)
-bf = sheet.cell_value(6, 2)
-bg = sheet.cell_value(7, 2)
-bh = sheet.cell_value(8, 2)
-bi = sheet.cell_value(9, 2)
-bj = sheet.cell_value(10, 2)
-b1 = sheet.cell_value(11, 2)
-b2 = sheet.cell_value(12, 2)
-b3 = sheet.cell_value(13, 2)
-
-ca =sheet.cell_value(1, 3)
-cb =sheet.cell_value(2, 3)
-cd = sheet.cell_value(4, 3)
-ce = sheet.cell_value(5, 3)
-cf = sheet.cell_value(6, 3)
-cg = sheet.cell_value(7, 3)
-ch = sheet.cell_value(8, 3)
-ci = sheet.cell_value(9, 3)
-cj = sheet.cell_value(10, 3)
-c1 = sheet.cell_value(11, 3)
-c2 = sheet.cell_value(12, 3)
-c3 = sheet.cell_value(13, 3)
-
-da =sheet.cell_value(1, 4)
-db =sheet.cell_value(2, 4)
-dc = sheet.cell_value(3, 4)
-de = sheet.cell_value(5, 4)
-df = sheet.cell_value(6, 4)
-dg = sheet.cell_value(7, 4)
-dh = sheet.cell_value(8, 4)
-di = sheet.cell_value(9, 4)
-dj = sheet.cell_value(10, 4)
-d1 = sheet.cell_value(11, 4)
-d2 = sheet.cell_value(12, 4)
-d3 = sheet.cell_value(13, 4)
-
-ea =sheet.cell_value(1, 5)
-eb =sheet.cell_value(2, 5)
-ec = sheet.cell_value(3, 5)
-ed = sheet.cell_value(4, 5)
-ef = sheet.cell_value(6, 5)
-eg = sheet.cell_value(7, 5)
-eh = sheet.cell_value(8, 5)
-ei = sheet.cell_value(9, 5)
-ej = sheet.cell_value(10, 5)
-e1 = sheet.cell_value(11, 5)
-e2 = sheet.cell_value(12, 5)
-e3 = sheet.cell_value(13, 5)
-
-fa =sheet.cell_value(1, 6)
-fb =sheet.cell_value(2, 6)
-fc = sheet.cell_value(3, 6)
-fd = sheet.cell_value(4, 6)
-fe = sheet.cell_value(5, 6)
-fg = sheet.cell_value(7, 6)
-fh = sheet.cell_value(8, 6)
-fi = sheet.cell_value(9, 6)
-fj = sheet.cell_value(10, 6)
-f1 = sheet.cell_value(11, 6)
-f2 = sheet.cell_value(12, 6)
-f3 = sheet.cell_value(13, 6)
-
-ga =sheet.cell_value(1, 7)
-gb =sheet.cell_value(2, 7)
-gc = sheet.cell_value(3, 7)
-gd = sheet.cell_value(4, 7)
-ge = sheet.cell_value(5, 7)
-gf = sheet.cell_value(6, 7)
-gh = sheet.cell_value(8, 7)
-gi = sheet.cell_value(9, 7)
-gj = sheet.cell_value(10, 7)
-g1 = sheet.cell_value(11, 7)
-g2 = sheet.cell_value(12, 7)
-g3 = sheet.cell_value(13, 7)
-
-ha =sheet.cell_value(1, 8)
-hb =sheet.cell_value(2, 8)
-hc = sheet.cell_value(3, 8)
-hd = sheet.cell_value(4, 8)
-he = sheet.cell_value(5, 8)
-hf = sheet.cell_value(6, 8)
-hg = sheet.cell_value(7, 8)
-hi = sheet.cell_value(9, 8)
-hj = sheet.cell_value(10, 8)
-h1 = sheet.cell_value(11, 8)
-h2 = sheet.cell_value(12, 8)
-h3 = sheet.cell_value(13, 8)
-
-ia =sheet.cell_value(1, 9)
-ib =sheet.cell_value(2, 9)
-ic = sheet.cell_value(3, 9)
-id = sheet.cell_value(4, 9)
-ie = sheet.cell_value(5, 9)
-ig = sheet.cell_value(7, 9)
-ih = sheet.cell_value(8, 9)
-ij = sheet.cell_value(10, 9)
-i1 = sheet.cell_value(11, 9)
-i2 = sheet.cell_value(12, 9)
-i3 = sheet.cell_value(13, 9)
-
-ja =sheet.cell_value(1, 10)
-jb =sheet.cell_value(2, 10)
-jc = sheet.cell_value(3, 10)
-jd = sheet.cell_value(4, 10)
-je = sheet.cell_value(5, 10)
-jf = sheet.cell_value(6, 10)
-jg = sheet.cell_value(7, 10)
-jh = sheet.cell_value(8, 10)
-ji = sheet.cell_value(9, 10)
-j1 = sheet.cell_value(11, 10)
-j2 = sheet.cell_value(12, 10)
-j3 = sheet.cell_value(13, 10)
+#2dmasīvs ar jautājumiem
+jautajumi = {
+  'a':{ 'jautajums':"Kurš ir koncertzāles'Lielais Dzintars' arhitekts?\na:Folkers Gīnke \nb:Gunars Birkerts\nc:Pauls Makss Berči", 'pareiza_atbilde': ['a']},
+  'b':{ 'jautajums':"Kādā krāsā ir Liepājas muzejs?\na:zils \nb:rozā\nc:zaļš", 'pareiza_atbilde': ['c']},
+  'c':{'jautajums':"Kā sauc šo zivi?\na:plekste\nb:menca\nc:līdaka", 'pareiza_atbilde': ['a'], 'jautajums2':"Kā sauc šo zivi?\na:asaris\nb:lasis\nc:rauda", 'pareiza_atbilde2':['b']},
+  'd':{'jautajums':"Kā vārdos ir nosaukti Liepājas tirgi?\na:Katrīnas un Jāņa \nb:Annas un Pētera\nc:Marijas un Jāņa", 'pareiza_atbilde':['b']},
+  'e':{'jautajums':"Kā sauc šo prezidentu(-i)?", 'pareiza_atbilde':["vaira vīķe freiberga","vaira vīķe-freiberga","vaira vīķe - freiberga","vaira vīķe -freiberga", "vaira vīķe -freiberga"], 'jautajums2':"Kā sauc šo prezidentu(-i)?", 'pareiza_atbilde2':['gustavs zemgals'], 'pareiza_atbilde3':['alberts kviesis']},
+  'g':{'jautajums':"Kādas ticības baznīca atrodas Karostā?\na:katoļu \nb:luterāņu\nc:pareizticīgo", 'pareiza_atbilde':['c']},
+  'h':{'jautajums':"Kura ir Grobiņas lielākā iela?\na:Brīvības iela \nb:Lielā iela\nc:Saules iela", 'pareiza_atbilde':['b']},
+  'i':{'jautajums':"Kādus dzīvniekus var novērot zirgu salā?\na:putnus \nb:zirgus\nc:roņus", 'pareiza_atbilde':['a']},
+  'j':{'jautajums':"Kādā secībā jāveic pirmā palīdzība?(atbilžu burtus atdali ar komatu)\na:sauc palīgā \nb:pārliecinies par savu un apkārtējo drošību.\nc:pārbaudi cietušā stāvokli", 'pareiza_atbilde':['b,c,a']}
+}
+print(jautajumi['a']['jautajums'])
 #laikapstākļu ģenerators
 import random
 apstakli=[2, 1]
@@ -162,6 +35,7 @@ if laikapstakli ==1:
 if laikapstakli == 2:
   print("Laikapstākļi ir slikti")
 
+global punkti
 punkti=10 
 
 #kontrolpunktu faila nolasīšana
@@ -177,54 +51,91 @@ with open('faili/kontrolpunkti.txt') as f:
 import docx
 doci = docx.Document('faili/Kontrolpunkts_muzejs.docx')
 #len(doci.paragraphs)
-print(doci.paragraphs[0].text)
-#'Kontrolpunkts_dzintars'
-print(doci.paragraphs[1].text)
-print(doci.paragraphs[2].text)
-print(doci.paragraphs[3].text)
-#'A plain paragraph with some bold and some italic'
+print(doci.paragraphs[0-3].text)
+#print(doci.paragraphs[1].text)
+#print(doci.paragraphs[2].text)
+#print(doci.paragraphs[3].text)
 #len(doci.paragraphs[1].runs)
 #print(doci.paragraphs[0].runs[0].text)
-'A plain paragraph with some '
-
-
-#'italic'
-#import textract
-#text = textract.process("faili/Kontrolpunkts_muzejs.docx")
-#print(text)
-#text = textract.process('faili/Kontrolpunkts_muzejs.docx', extension='docx')
-
 #open excel
 import pandas as pd
 df = pd.read_excel (r'faili/kontrolpunktu tabula (bez km).xlsx')
 data=pd.read_excel (r'faili/kontrolpunktu tabula (bez km).xlsx')
 dklk = pd.DataFrame(data, columns= ['a','b'])
 print (df)
-identifikators = input("Uz kuru vietu dosies? Ievadi kontorpukta identifikatoru: ")
-if identifikators == "ab":
-  cels = ab
-
-
-
-#aprēķina akumulatora izlādi atkarībā no laikapstākļiem
-akumulators_sakuma = 100
-      #uztaisīt, lai reālus km var mainīt 
-if laikapstakli == 1 and int(cels)<=140:
-    akumulators_izteretais= cels*100/140 
+import xlrd
+ 
+# Give the location of the file
+loc = ("faili/kontrolpunktu tabula (bez km).xlsx")
+ 
+# To open Workbook
+wb = xlrd.open_workbook(loc)
+sheet = wb.sheet_by_index(0)
+print(sheet.cell_value(1,1))
+#vietas indeksi
+indeksi = {
+  'a':1,'b':2,'c':3,'d':4, 'e':5, 'f':6, 'g':7,'h':8,'i':9,'j':10,'1': 11, '2':12, '3':14
+  }
+akumulators = 100
+def izlade_akumulators(laikapstakli,cels):
+  #uztaisīt, lai reālus km var mainīt
+  if laikapstakli == 1 and int(cels)<=140:
+    akumulators_izteretais = cels*100/140 
     print(akumulators_izteretais)
-if laikapstakli==1 and int(cels)>140:
-    akumulators_izteretais=akumulators_sakuma
+  if laikapstakli==1 and int(cels)>140:
+    akumulators_izteretais=akumulators
     print("Akumulators izlādējies")
-if laikapstakli == 2 and int(cels)<=80:
+  if laikapstakli == 2 and int(cels)<=80:
     akumulators_izteretais= cels*100/80 
     print(akumulators_izteretais)
-if laikapstakli==2 and int(cels)>80:
-    akumulators_izteretais=akumulators_sakuma
+  if laikapstakli==2 and int(cels)>80:
+    akumulators_izteretais= akumulators
     print("Akumulators izlādējies")
+  return akumulators_izteretais
+#spēles jēga
+akumulators = 100
+kontrolpunkti = 0
 
-akumulators = round(akumulators_sakuma-akumulators_izteretais)#round NUMBERS!!!
-kilometri = 140*akumulators/100
-print("uzlādes līmenis ir "+str(akumulators)+"%\nvēl var nobraukt "+str(kilometri)+" km")
+while kontrolpunkti < 10: 
+  vieta_tagad= "a"
+  vieta = input("Uz kurieni dosies? Ievadi atbistošo kontrolpunkta burtu: ")
+  #aprēķina akumulatora izlādi atkarībā no laikapstākļiem
+
+
+  if vieta == 'pārbaudīt līmeni':
+    izlade_akumulators(laikapstakli,cels)
+  if vieta in jautajumi:
+    cels = sheet.cell_value(indeksi[vieta_tagad],indeksi[vieta])
+    print(cels)
+    kontrolpunkti = kontrolpunkti+1
+    akumulators = round(akumulators-izlade_akumulators(laikapstakli, cels))
+    kilometri = 140*akumulators/100
+    print("uzlādes līmenis ir "+str(akumulators)+"%\nvēl var nobraukt "+str(kilometri)+" km")
+    #if atbilde in jautajumi[vieta]['pareiza_atbilde']:
+     # punkti=punkti+10
+      #print("Pareizi!")
+    print(jautajumi[vieta]['jautajums'])
+    atbilde = input()
+    k=0
+    while atbilde not in jautajumi[vieta]['pareiza_atbilde']:
+      k+=1
+      print("Mēģini vēlreiz!")
+      print(jautajumi[vieta]['jautajums'])
+      atbilde = input()
+    punkti+=10/(k+1) if k<2 else 0
+
+
+  elif vieta == 'f':
+    print("bla bla bla")
+    
+
+   
+
+  #if input("ievadi a")=="a":
+    #kontrolpunkti= kontrolpunkti+1
+  
+
+
 
 
 #jāuzlabo, lai vienmēr var paskatīties stāvokli
